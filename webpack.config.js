63508d6562e6ed2ec8bf218d8294b8cb7d67f36a
Bartek Dybowski <bartlomiej.dybowski@goingapp.pl>
2019-09-10 18:18:21 +0200

    Style linting with checking before commit added

--- webpack.config.js
+++ webpack.config.js
@@ -4,34 +4,42 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'development',
-  devtool: "cheap-module-inline-source-map",
+  devtool: 'cheap-module-inline-source-map',
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
   },
   entry: {
-    bundle: './src/index.tsx'
+    bundle: './src/index.tsx',
   },
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"],
+    extensions: ['.ts', '.tsx', '.js'],
   },
   module: {
     rules: [
-      { test: /\.tsx?$/, enforce: "pre", loader: "tslint-loader" },
-      { test: /\.tsx?$/, loader: "ts-loader" },
-      { test: /\.s[ac]ss$/i, use: [ 'style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ] },
+      { test: /\.tsx?$/, enforce: 'pre', loader: 'tslint-loader' },
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "index.html"
+      template: 'index.html',
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css',
       ignoreOrder: false,
     }),
-  ]
+  ],
 };

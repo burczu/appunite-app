493dda607826951b9d925b8146c6c630e07ebad5
Bartek Dybowski <bartlomiej.dybowski@goingapp.pl>
2019-09-10 19:19:39 +0200

    Preparing aliases wrong path fixed

--- utils/functions.js
+++ utils/functions.js
@@ -4,7 +4,7 @@ const prepareAliases = (paths) => {
   const alias = {};
 
   Object.keys(paths).forEach((key) => {
-    alias[key] = path.resolve(__dirname, '../../', paths[key]);
+    alias[key] = path.resolve(__dirname, '../', paths[key]);
   });
 
   return alias;

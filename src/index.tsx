dc19cbec7890423990c2104ee63e553d26ea643a
Bartek Dybowski <bartlomiej.dybowski@goingapp.pl>
2019-09-10 19:24:07 +0200

    Entry point rewriten to use Redux store and the App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './index.scss';
 
-render(<div className="test">It's alive!</div>, document.getElementById(
-  'root',
-) as HTMLElement);
+import configureStore from '@/store';
+import App from '@Compo/App';
+import { Provider } from 'react-redux';
+
+const store = configureStore();
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root') as HTMLElement,
+);

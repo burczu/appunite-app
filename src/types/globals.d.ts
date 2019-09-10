aa396362ce40d1b7940a911b4c1fd85180241722
Bartek Dybowski <bartlomiej.dybowski@goingapp.pl>
2019-09-10 19:21:15 +0200

    Global types added

---
+++ src/types/globals.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable: interface-name */
+
+declare interface Window {
+  __REDUX_DEVTOOLS_EXTENSION__: any;
+  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any;
+
+  store: any;
+}
+
+declare interface NodeModule {
+  hot: {
+    accept: (path?: string, callback?: any) => void;
+  };
+}

22031a7c3ee861067865280f0df7b50aaeca3c65
Bartek Dybowski <bartlomiej.dybowski@goingapp.pl>
2019-09-11 15:29:14 +0200

    Alias configuration for Jest

--- jest.config.js
+++ jest.config.js
@@ -11,4 +11,13 @@ module.exports = {
   "setupFilesAfterEnv": [
     "<rootDir>/src/setupTests.ts"
   ],
+  "moduleNameMapper": {
+    "^@/(.*)$": '<rootDir>/src/$1',
+    "^@Compo/(.*)$": '<rootDir>/src/components/$1',
+    "^@Config/(.*)$": '<rootDir>/src/config/$1',
+    "^@Misc/(.*)$": '<rootDir>/src/misc/$1',
+    "^@Model/(.*)$": '<rootDir>/src/models/$1',
+    "^@Services/(.*)$": '<rootDir>/src/services/$1',
+    "^@Styles/(.*)$": '<rootDir>/src/styles/$1',
+  }
 };

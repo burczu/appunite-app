04fb33b89d183dcfdcd6ff9f91496aad66d07f49
Bartek Dybowski <bartlomiej.dybowski@goingapp.pl>
2019-09-12 16:42:24 +0200

    Dates filter actions added

--- src/models/filters/actions/index.ts
+++ src/models/filters/actions/index.ts
@@ -1,11 +1,13 @@
 import { createStandardAction } from 'typesafe-actions';
 import {
   _SET_CATEGORY,
+  _SET_DATE,
   _SET_SORT_BY,
   SET_CATEGORY_FILTER,
+  SET_DATE_FILTER,
   SET_SORT_BY_FILTER,
 } from './../constants/actions';
-import { IFiltersSortBy } from './../types';
+import { IFiltersDates, IFiltersSortBy } from './../types';
 
 export const setCategoryFilter = createStandardAction(SET_CATEGORY_FILTER)<
   string
@@ -15,10 +17,18 @@ export const setSortByFilter = createStandardAction(SET_SORT_BY_FILTER)<
   IFiltersSortBy
 >();
 
+export const setDateFilter = createStandardAction(SET_DATE_FILTER)<
+  IFiltersDates
+>();
+
 export const setCategory = createStandardAction(_SET_CATEGORY)<
   string | undefined
 >();
 
 export const setSortBy = createStandardAction(_SET_SORT_BY)<
   IFiltersSortBy | undefined
+>();
+
+export const setDate = createStandardAction(_SET_DATE)<
+  IFiltersDates | undefined
 >();
--- src/models/filters/constants/actions.ts
+++ src/models/filters/constants/actions.ts
@@ -1,7 +1,9 @@
 // Private action creators
 export const _SET_CATEGORY = 'NewsApp/filters/_SET_CATEGORY';
 export const _SET_SORT_BY = 'NewsApp/filters/_SET_SORT_BY';
+export const _SET_DATE = 'NewsApp/filters/_SET_DATE';
 
 // Public action creators
 export const SET_CATEGORY_FILTER = 'NewsApp/filters/SET_CATEGORY_FILTER';
 export const SET_SORT_BY_FILTER = 'NewsApp/filters/SET_SORT_BY_FILTER';
+export const SET_DATE_FILTER = 'NewsApp/filters/SET_DATE_FILTER';

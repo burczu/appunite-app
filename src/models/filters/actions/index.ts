de868221f2863eb95518afa6a02607639eafc464
Bartek Dybowski <bartlomiej.dybowski@goingapp.pl>
2019-09-12 15:16:27 +0200

    SortBy actions added

--- src/models/filters/actions/index.ts
+++ src/models/filters/actions/index.ts
@@ -1,10 +1,24 @@
 import { createStandardAction } from 'typesafe-actions';
-import { _SET_FILTER, SET_CATEGORY_FILTER } from './../constants/actions';
+import {
+  _SET_FILTER,
+  _SET_SORT_BY,
+  SET_CATEGORY_FILTER,
+  SET_SORT_BY_FILTER,
+} from './../constants/actions';
+import { IFiltersSortBy } from './../types';
 
 export const setCategoryFilter = createStandardAction(SET_CATEGORY_FILTER)<
   string
 >();
 
+export const setSortByFilter = createStandardAction(SET_SORT_BY_FILTER)<
+  IFiltersSortBy
+>();
+
 export const setFilter = createStandardAction(_SET_FILTER)<
   string | undefined
+>();
+
+export const setSortBy = createStandardAction(_SET_SORT_BY)<
+  IFiltersSortBy | undefined
 >();
--- src/models/filters/constants/actions.ts
+++ src/models/filters/constants/actions.ts
@@ -1,5 +1,7 @@
 // Private action creators
 export const _SET_FILTER = 'NewsApp/filters/_SET_FILTER';
+export const _SET_SORT_BY = 'NewsApp/filters/_SET_SORT_BY';
 
 // Public action creators
 export const SET_CATEGORY_FILTER = 'NewsApp/filters/SET_CATEGORY_FILTER';
+export const SET_SORT_BY_FILTER = 'NewsApp/filters/SET_SORT_BY_FILTER';
